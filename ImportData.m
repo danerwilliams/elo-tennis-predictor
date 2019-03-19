f55bd78e82f0049a6de894db6a2dc26c470f04b5
Dane Williams <dwilli36@nd.edu>
2019-03-19 11:08:06 -0400

    added import data function and main

---
+++ ImportData.m
@@ -0,0 +1,8 @@
+%% ImportData.m
+
+% Team Members: Dane Williams, Matt Dillane, Billy Porter, Michael Pittz
+
+% function which imports elo tennis data from excel file(s) and returns an
+% array with win loss data for matches to be used for manipulation
+
+function wldata = ImportData(excel)
---
+++ main.m
@@ -0,0 +1,5 @@
+%% main.m
+
+% Team Members: Dane Williams, Billy Porter, Matt Dillane, Michael Pitz
+
+% primary script, used to call functions and run program
