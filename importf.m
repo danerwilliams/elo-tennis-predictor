7153bc79eceeb63f75bd5ce87c21eeaf7d4519a3
Dane Williams <dwilli36@nd.edu>
2019-04-09 19:34:38 -0400

    edited excel files and changed importData to include matches

--- importf.m
+++ importf.m
@@ -8,7 +8,7 @@
 function data = importf(file) %parameter is path name as a string
 
 [~, ~, data] = xlsread(file);
-data = data(1:length(data),3:11);
+data = data(1:length(data),3:10);
 data(cellfun(@(x) ~isempty(x) && isnumeric(x) && isnan(x),data)) = {''};
 
 idx = cellfun(@ischar, data);
