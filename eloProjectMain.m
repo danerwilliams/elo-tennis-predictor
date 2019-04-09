447353b0ff83991db5c6def8c3894b8372ce1863
jamie heneghan <jheneghan16@gmail.com>
2019-04-09 18:51:54 -0400

    integrated w woodys thing

--- eloProjectMain.m
+++ eloProjectMain.m
@@ -8,43 +8,40 @@ clc
 %% Create User IDs
 
 
-%{
+
 %implementation of ImportData function to generate a struct of data from
 %all files entered as arguments
 
-years = {'2001.xlsx', '2002.xlsx','2003.xlsx','2004.xlsx', ...
-    '2005.xlsx','2006.xlsx','2007.xlsx','2008.xlsx', ...
-    '2009.xlsx','2010.xlsx','2011.xlsx','2012.xlsx', ...
+years = {'2010.xlsx','2011.xlsx','2012.xlsx', ...
     '2013.xlsx','2014.xlsx','2015.xlsx','2016.xlsx', ...
     '2017.xlsx','2018.xlsx','2019.xlsx'};
 
 [data, unique] = ImportData(years);
-%}
+
 
 % another test case
 %[data, unique] = ImportData({'2017.xlsx', '2018.xlsx', '2019.xlsx'});
 
-a = 1:1:197;
+a = 1:1:length(unique);
 x = a.';
-b = table2cell(playerNames(:,2));
-names = string(b.');
+names = unique.';
 name2IDmap = containers.Map('KeyType','char','ValueType','double');
-for i = 1:197
-    name2IDmap(char(names(1,i))) = i;
+for i = 1:length(unique)
+    name2IDmap(char(names(i,1))) = i;
 end
-ID2namemap = containers.Map(x,b);
+ID2namemap = containers.Map(x,unique);
 
 
 %% Initialize every players ELO to 1500
-playerELOs = zeros(197,1);
+playerELOs = zeros(length(unique),1);
 playerELOs(:,1) = 1500;
 
 
 %% update ELOs from match data
-for i = 1:length(matchdata)
-    winner = char(string(matchdata{i,10}));
+for i = 1:length(data)
+    winner = char(data(i).Winner);
     winnerID = name2IDmap(winner);
-    loser = char(string(matchdata{i,11}));
+    loser = char(data(i).Loser);
     loserID = name2IDmap(loser);
     [playerELOs(winnerID),playerELOs(loserID)] =(calculateElo(playerELOs(winnerID),32,1,playerELOs(loserID)));
     
