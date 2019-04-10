4a5ee0e2977d50f1bc62f91bd2dabc6013b609c8
jamie heneghan <jheneghan16@gmail.com>
2019-04-09 21:30:18 -0400

    Added comments, just for you Billy <3

--- calculateElo.m
+++ calculateElo.m
@@ -4,25 +4,26 @@ function [newPlayerOneElo,newPlayerTwoElo]=calculateElo(playerOneID,playerTwoID)
 % K = K factor (Max amount of points gained or lost)
 % Outcome = 1 or 0, corresponding to Win or Loss
 % opponentElo = the opposing players Elo before the game
-    global playerELOs;
+    global playerELOs;          %Allows access to playerELOs and playerMatches
     global playerMatches;
 
-    playerOneElo = playerELOs(playerOneID,1);
+    playerOneElo = playerELOs(playerOneID,1);       %Gets the old ELOs of the players
     playerTwoElo = playerELOs(playerTwoID,1);
     
-    pWinPlayerOne = 1/(1 + 10^((playerOneElo - playerTwoElo)/400));
+    pWinPlayerOne = 1/(1 + 10^((playerOneElo - playerTwoElo)/400)); %Calculates win probabilities
     pWinPlayerTwo = 1/(1 + 10^((playerTwoElo - playerOneElo)/400));
     
     
-    playerOneMatches = playerMatches(playerOneID);
+    playerOneMatches = playerMatches(playerOneID);      %gets how many matches in a players data set
     playerTwoMatches = playerMatches(playerTwoID);
     
-    kPlayerOne = ((25)/(playerOneMatches + 5)^.4);
+    kPlayerOne = ((25)/(playerOneMatches + 5)^.4);      %calculates K based on matches in set
     kPlayerTwo = ((25)/(playerTwoMatches + 5)^.4);
     
-    playerOneOutcome = 1;
+    playerOneOutcome = 1;       %Winner is 1, loser is 0
     playerTwoOutcome = 0;
     
+    %Calculates ELOs based on ELO formula
     newPlayerOneElo = round(playerOneElo + kPlayerOne*(playerOneOutcome - pWinPlayerOne),2);
     
     newPlayerTwoElo = round(playerTwoElo + kPlayerTwo*(playerTwoOutcome - pWinPlayerTwo),2);
--- eloProjectMain.m
+++ eloProjectMain.m
@@ -20,29 +20,30 @@ years = {'2010.xlsx','2011.xlsx','2012.xlsx','2013.xlsx','2014.xlsx','2015.xlsx'
 % another test case
 %[data, unique] = ImportData({'2017.xlsx', '2018.xlsx', '2019.xlsx'});
 
+
 a = 1:1:length(unique);
-x = a.';
-names = unique.';
+x = a.';                    % Creates vector with an index for every unique player
+names = unique.';           % Transposes the vector with all the unique names
 name2IDmap = containers.Map('KeyType','char','ValueType','double');
-for i = 1:length(unique)
+for i = 1:length(unique)        %Creates maps for names and IDs. Ex( name2IDmap('Federer R.') gives Federer's ID
     name2IDmap(char(names(i,1))) = i;
 end
 ID2namemap = containers.Map(x,unique);
 
 
 %% Initialize every players ELO to 1500
-global playerELOs;
-global playerMatches;
+global playerELOs;          %Declares playerELOs and playerMatches as global,
+global playerMatches;       % so calculateELO.m can access it
 
-playerELOs = zeros(length(unique),1);
-playerELOs(:,1) = 1500;
-playerMatches = zeros(length(unique),1);
+playerELOs = zeros(length(unique),1); %Initializes a vector for all ELOs 
+playerELOs(:,1) = 1500;                 %Initializes all ELOs to 1500
+playerMatches = zeros(length(unique),1); %Initializes vector of matches played for each player
 
 
 %% Find how many matches in a player's dataset
 
-for i = 1:length(data)
-    winner = char(data(i).Winner);
+for i = 1:length(data)                  %Loops through all matches, and counts for each player 
+    winner = char(data(i).Winner);      % how many matches they have played
     winnerID = name2IDmap(winner);
     loser = char(data(i).Loser);
     loserID = name2IDmap(loser);
@@ -51,8 +52,8 @@ for i = 1:length(data)
 end
 
 %% update ELOs from match data
-for i = 1:length(data)
-    winner = char(data(i).Winner);
+for i = 1:length(data)                  %Loops through all matches, and updates player's
+    winner = char(data(i).Winner);      % ELOs for each match
     winnerID = name2IDmap(winner);
     loser = char(data(i).Loser);
     loserID = name2IDmap(loser);
@@ -60,4 +61,4 @@ for i = 1:length(data)
     
 end
 
-playerELOs = round(playerELOs);
+playerELOs = round(playerELOs);         %Rounds playerELOs for readability
