7273479117dc0f28dff11640653ec79a3982f217
jamie heneghan <jheneghan16@gmail.com>
2019-04-09 21:22:20 -0400

    Edited calculateElo.m to just take the player IDs of the winners and losers. Added K value based on number of matches played, which is calculated inside of calculateElo.m

--- calculateElo.m
+++ calculateElo.m
@@ -1,18 +1,31 @@
-function [newPlayerOneElo,newPlayerTwoElo]=calculateElo(oldPlayerOneElo,K,playerOneOutcome,oldPlayerTwoElo)
+function [newPlayerOneElo,newPlayerTwoElo]=calculateElo(playerOneID,playerTwoID)
 % CALCULATE ELO
 % oldElo = Players ELO ranking before the match occurs
 % K = K factor (Max amount of points gained or lost)
 % Outcome = 1 or 0, corresponding to Win or Loss
 % opponentElo = the opposing players Elo before the game
-    pWinPlayerOne = 1/(1 + 10^((oldPlayerOneElo - oldPlayerTwoElo)/400));
-    pWinPlayerTwo = 1/(1 + 10^((oldPlayerTwoElo - oldPlayerOneElo)/400));
+    global playerELOs;
+    global playerMatches;
 
-    newPlayerOneElo = round(oldPlayerOneElo + K*(playerOneOutcome - pWinPlayerOne),2);
-    if (playerOneOutcome)
-        playerTwoOutcome = 0;
-    else playerTwoOutcome = 1;
-    end
-    newPlayerTwoElo = round(oldPlayerTwoElo + K*(playerTwoOutcome - pWinPlayerTwo),2);
+    playerOneElo = playerELOs(playerOneID,1);
+    playerTwoElo = playerELOs(playerTwoID,1);
+    
+    pWinPlayerOne = 1/(1 + 10^((playerOneElo - playerTwoElo)/400));
+    pWinPlayerTwo = 1/(1 + 10^((playerTwoElo - playerOneElo)/400));
+    
+    
+    playerOneMatches = playerMatches(playerOneID);
+    playerTwoMatches = playerMatches(playerTwoID);
+    
+    kPlayerOne = ((25)/(playerOneMatches + 5)^.4);
+    kPlayerTwo = ((25)/(playerTwoMatches + 5)^.4);
+    
+    playerOneOutcome = 1;
+    playerTwoOutcome = 0;
+    
+    newPlayerOneElo = round(playerOneElo + kPlayerOne*(playerOneOutcome - pWinPlayerOne),2);
+    
+    newPlayerTwoElo = round(playerTwoElo + kPlayerTwo*(playerTwoOutcome - pWinPlayerTwo),2);
 
 end
     
--- eloProjectMain.m
+++ eloProjectMain.m
@@ -12,7 +12,7 @@ clc
 %implementation of ImportData function to generate a struct of data from
 %all files entered as arguments
 
-years = {'2014.xlsx','2015.xlsx','2016.xlsx','2017.xlsx', '2018.xlsx', '2016.xlsx','2019.xlsx'};
+years = {'2010.xlsx','2011.xlsx','2012.xlsx','2013.xlsx','2014.xlsx','2015.xlsx','2016.xlsx','2017.xlsx', '2018.xlsx', '2016.xlsx','2019.xlsx'};
 
 [data, unique] = ImportData(years);
 
@@ -31,9 +31,24 @@ ID2namemap = containers.Map(x,unique);
 
 
 %% Initialize every players ELO to 1500
+global playerELOs;
+global playerMatches;
+
 playerELOs = zeros(length(unique),1);
 playerELOs(:,1) = 1500;
+playerMatches = zeros(length(unique),1);
 
+
+%% Find how many matches in a player's dataset
+
+for i = 1:length(data)
+    winner = char(data(i).Winner);
+    winnerID = name2IDmap(winner);
+    loser = char(data(i).Loser);
+    loserID = name2IDmap(loser);
+    playerMatches(winnerID) = playerMatches(winnerID) + 1;
+    playerMatches(loserID) = playerMatches(loserID) + 1;
+end
 
 %% update ELOs from match data
 for i = 1:length(data)
@@ -41,7 +56,7 @@ for i = 1:length(data)
     winnerID = name2IDmap(winner);
     loser = char(data(i).Loser);
     loserID = name2IDmap(loser);
-    [playerELOs(winnerID),playerELOs(loserID)] =(calculateElo(playerELOs(winnerID),3,1,playerELOs(loserID)));
+    [playerELOs(winnerID),playerELOs(loserID)] =(calculateElo(winnerID,loserID));
     
 end
 
