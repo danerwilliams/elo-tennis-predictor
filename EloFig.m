951f193243df1f5e4a831a815777f1e6f93b2d84
Billy Porter <wporter2@nd.edu>
2019-04-24 22:38:13 -0400

    GAH

--- EloFig.m
+++ EloFig.m
@@ -182,3 +182,5 @@ function DecisionButton_Callback(hObject, eventdata, handles)
 % hObject    handle to DecisionButton (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+
--- plotK.m
+++ plotK.m
@@ -13,10 +13,10 @@ load('datadata.mat');
 %plot(kVector(1:13),percentVector(1:13),'b',kVector(5),percentVector(5),'ro');
 
 for i = 1:13
-    plot(handles.Kaxes, kVector(1:i),percentVector(1:i),'b',kVector(i),percentVector(i),'ro');
-    xlabel('K');
-    ylabel('Percent Correct');
-    axis([5 55 61 71])
+    plot(handles.Kaxes, kVector(1:i),percentVector(1:i),'b',kVector(i),percentVector(i),'k.');
+    xlabel(handles.Kaxes,'K');
+    ylabel(handles.Kaxes,'Percent Correct');
+    axis(handles.Kaxes,[5 55 61 71])
     
     grid on
     drawnow
