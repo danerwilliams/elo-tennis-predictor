279fee572f550d09c359780444814c0eedb1461a
jamie heneghan <jheneghan16@gmail.com>
2019-04-30 11:01:55 -0400

    scaled ELO to make more accurate probabilities.

--- eloProjectMain.m
+++ eloProjectMain.m
@@ -16,8 +16,19 @@ years = {'2010.xlsx', '2011.xlsx', '2012.xlsx', '2013.xlsx', '2014.xlsx', '2015.
 
 [playerELOs, playerMatches] = InitializeElos(data, unique, name2IDmap);
 
+for i = 1:282
+    scaledDifference = playerELOs(i) - 1500;
+    playerELOs(i) = 1.5 * scaledDifference + 1500;
+end
+
+
 [k, kVector, percentVector] = optimizeK(data, unique, name2IDmap);
 
 [playerELOs, federerELOs, murrayELOs, djokovicELOs, nadalELOs] = SetElos(data, name2IDmap, k, playerELOs, playerMatches);
 federerELOs = round(federerELOs);
+
+for i = 1:282
+    scaledDifference = playerELOs(i) - 1500;
+    playerELOs(i) = 3.5 * scaledDifference + 1500;
+end
 
